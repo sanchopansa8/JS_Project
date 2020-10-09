21cd8d7db44f42ae6df433f87966bd9a46b0b382
Rogovenko Alexandra <workalexro@gmail.com>
2020-10-09 18:31:10 +0300

    added function to change privat field and modified writeYourGenres function

--- js/script.js
+++ js/script.js
@@ -1,63 +1,65 @@
 "use strict";
-let numberOfFilms;
 
-function start() {
-   
-    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("How many films have you watched?", "");
-    }
+function toggleVisibleMyDB(){
+    personalMovieDB.privat = !personalMovieDB.privat;
 }
 
-start();
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        this.count = prompt("How many films have you watched?", "");
+    },
+    rememberMyFilms: function (){
+        for (let i = 0; i < 2;) {
+            const film = prompt("One of recently wathed movies?", "");
+            const filmMark = prompt("How would you assess it?", "");
+            if(film != null && filmMark != null && film != "" && filmMark != "" && film.length < 50){
+                personalMovieDB.movies[film] = filmMark;
+                i++;
+            }
+        }
+    },
+    detectPersonalLevel: function(){
+        if(personalMovieDB.count < 10){
+            console.log("Not many films");
+        } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            console.log("Classic viewer");
+        }else if(personalMovieDB.count >= 30){
+            console.log("You are cinemaddict!");
+        }else {
+            console.log("Error occured");
+        }
+    },
+    showMyDB: function (){
+        if(!personalMovieDB.privat){
+             console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function(){
+        for(let i = 1; i < 4; i++){
+            let input = prompt(`Your favourite genre # ${i}?`);
+            if(input === null || input === ""){
+                i--;
+            } else {
+                personalMovieDB.genres[i-1] = input;
+            }
+        }
+
+        this.genres.forEach((element, i, arr) => {
+            console.log(`Favourite genre # ${i+1} - is ${element}`);
+        });
+    }
 };
-function rememberMyFilms(){
-for (let i = 0; i < 2;) {
-    const film = prompt("One of recently wathed movies?", "");
-    const filmMark = prompt("How would you assess it?", "");
-    if(film != null && filmMark != null && film != "" && filmMark != "" && film.length < 50){
-        personalMovieDB.movies[film] = filmMark;
-        i++;
-    }
-}
-}
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB();
+ personalMovieDB.writeYourGenres();
+toggleVisibleMyDB();
 
-rememberMyFilms();
-
-function detectPersonalLevel(){
-if(personalMovieDB.count < 10){
-    console.log("Not many films");
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log("Classic viewer");
-}else if(personalMovieDB.count >= 30){
-    console.log("You are cinemaddict!");
-}else {
-    console.log("Error occured");
-}
-}
-
-detectPersonalLevel();
-
-
-function showMyDB(){
-    if(personalMovieDB.privat == false){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB();
-
-function writeYourGenres(){
-    for(let i = 1; i < 4; i++){
-       let genre = prompt("Your favourite genre #" + i);
-       personalMovieDB.genres[i-1] = genre;
-    }
-}
-
-writeYourGenres();
+console.log(personalMovieDB.count);
 
